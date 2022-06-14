4e61c702b363823d9c5e82de3879b2c57ac9af03
Alex Shevlin <alexshevlin1@gmail.com>
2022-06-14 10:30:40 -0700

    add conditional pricing for age, membership, and student status.
    Co-authored-by: Seung Lee <shl7@uw.edu>

--- js/scripts.js
+++ js/scripts.js
@@ -9,12 +9,69 @@ day (M-S)
 base price
 */
 
-function Ticket(name, rating, ) {
+function Ticket(name, rating, age, time, day, isStudent, isMember) {
   this.name = name;
   this.rating = rating;
+  this.price = 8;
+  this.age = age;
+  this.time = time;
+  this.day = day;
+  this.isStudent = isStudent;
+  this.isMember = isMember;
 }
 
+/*
+method that calculates the price based on age
+  child/senior tickets = cheapest same price 
+    8     60
+  student = slightly cheaper price
+  adult = most expensive
+
+  extended goal(?)
+  change price based on size of screen and stuff (IMAX 3D, IMAX 2D, 4D, REALD 3D, Digital Cinema, membership)
+    stacking discounts?
+*/
+Ticket.prototype.calculateTicketPrice = function() {
+  let output = [this.price, ""];
+  if (this.age <= 8 || this.age >= 60) {
+    output[0] -= 2;
+    output[1] = "Congratulations, you get a discount due to your age!";
+  }
+  if (this.isStudent) {
+    output[0] -= 1;
+    output[1] = "Congratulations, you get a student discount!";
+  }  
+  if (this.isMember) {
+    output[0] -= 1;
+    output[1] = "Congratulations, you get a membership discount!";
+  }
+  if (this.isMember && this.isStudent) {
+    output[0] += 1;
+    output[1] = "Sorry you can't qualify for both of these discounts."
+  }
+  if (this.price - 3 === price) {
+    output[1] = "Congratulations, you qualify for two discounts!";
+  }
+  if (this.price - 4 === price && this.isMember) {
+    output[0] += 5;
+    output[1] = "Are you really a member, student, and " + this.age + " years old?";
+  }
+  return output;
+};
+
 const movies = [
+  {
+    title: "Dr. Strange in the Multiverse of Madness", 
+    rating: "PG-13"
+  },
+]
+let ticket01 = new Ticket(movies[0].title, movies[0].rating, 27, '8:30 PM', 'Tuesday', true, false);
+console.log(ticket01.calculateTicketPrice());
+
+
+
+// User Interface Logic
+const moviesMain = [
   {
     title: "Dr. Strange in the Multiverse of Madness", 
     rating: "PG-13"
@@ -36,24 +93,6 @@ const movies = [
     rating: "R"
   },
 ]
-
-Ticket.prototype.calculateTicketPrice = function() {
-  
-}
-
-/*
-method that calculates the price based on age
-  child/senior tickets = cheapest same price 
-    8     60
-  student = slightly cheaper price
-  adult = most expensive
-
-  extended goal(?)
-  change price based on size of screen and stuff (IMAX 3D, IMAX 2D, 4D, REALD 3D, Digital Cinema, membership)
-    stacking discounts?
-*/
-
-// User Interface Logic
 
 $(document).ready(function() {
   
